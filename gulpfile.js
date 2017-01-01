bab278b344df5a720e0701443e6911681c580987
Robin Westerhoff <rwesterhoff@me.com>
2017-01-01 22:10:40 +0100

    Added navigation menu + basic styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var prefix = require('gulp-autoprefixer');
 
 var paths = {
     scss: 'src/sass/**/*.scss',
     css: 'src/css',
     html: 'src/*.html'
 }
-
-/*gulp.task('sync', function() {
-    bs.init({
-        server: {
-            baseDir: "src"
-        }
-    });
-});
-
-gulp.task('sass', function() {
-    return gulp.src(paths.scss)
-        .pipe(sass())
-        .pipe(gulp.dest(paths.css));
-});
-
-gulp.task('watch', ['browser-sync'], function () {
-    gulp.watch("src/sass/*.scss", ['sass']);
-    gulp.watch("src/*.html").on('change', bs.reload);
-});
-
-
-gulp.task('watch', function() {
-    gulp.watch(paths.scss, ['sass', 'sync']);
-});
-
-gulp.task('default', ['sass', 'sync', 'watch']);
-*/
-
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -51,6 +24,10 @@ gulp.task('serve', ['sass'], function() {
 gulp.task('sass', function() {
     return gulp.src(paths.scss)
         .pipe(sass())
+        .pipe(prefix({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest(paths.css))
         .pipe(browserSync.stream());
 });
